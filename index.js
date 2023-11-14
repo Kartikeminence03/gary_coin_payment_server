a973b4612c61a1c44c51dac5896c72d168c8c82e
Kartikemenence03 <kartik.eminence@gmail.com>
2023-11-14 11:05:16 +0530

    update

--- index.js
+++ index.js
@@ -9,18 +9,36 @@ app.use(cors());
 
 
 app.post("/api/create-checkout-session",async(req,res)=>{
-    const {products} = req.body;
-
-    const lineItems = products.map((product)=>({
-        price_data:{
-            currency:"inr",
-            product_data:{
-                name:product.dish,
+    try {
+        const {products} = req.body;
+        // console.log(products);
+    
+        const lineItems = products.map((product)=>({
+            price_data:{
+                currency:"inr",
+                product_data:{
+                    name:product.tokenPrice,
+                },
+                unit_amount:123 * 100,
             },
-            unit_amount:product.price * 100,
-        },
-        quantity:product.qnty
-    }))
+            quantity:product.quant
+        }))
+    
+        // console.log({ lineItems })
+    
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types:["card"],
+            line_items:lineItems,
+            mode:"payment",
+            success_url:"http://localhost:3000/sucess",
+            cancel_url:"http://localhost:3000/cancel",
+        });
+    
+        res.json({id:session.id})
+    } catch (error) {
+        console.log({ error: error.raw.message })
+    }
+
 })
 
 app.listen(7000,()=>{
